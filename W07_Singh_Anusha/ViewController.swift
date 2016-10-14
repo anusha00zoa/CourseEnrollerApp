aab9a6367c50daa289ebf5b191336392ecf56462
anusha00zoa <anusha00zoa@gmail.com>
2016-10-14 11:52:38 -0500

    Initial Commit

---
+++ W07_Singh_Anusha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  W07_Singh_Anusha
+//
+//  Created by SINGH ANUSHA  on 10/14/16.
+//  Copyright © 2016 SINGH ANUSHA . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
